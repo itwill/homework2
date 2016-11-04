323232d9cfcd53f7b04e7b4d3a7f45c737a6e657
Svetlana Chayka <probnii@mail.ru>
2016-11-05 00:12:32 +0500

    First commit

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Домашнее задание №2</title>
+</head>
+<body>
+<h3>Задание 1</h3>
+<?php
+echo "Работа с функциями 1" . PHP_EOL;
+echo "Работа с функциями 2" . PHP_EOL;
+?>
+</body>
+</html>
